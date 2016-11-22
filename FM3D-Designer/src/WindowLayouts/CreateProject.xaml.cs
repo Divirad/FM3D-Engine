48b89ee3f6f8bfa154dac3377693e8a4825a2919
PonySchreck <max98tonton@gmail.com>
2016-11-22 19:21:23 +0100

    Projekt kann erstellt werden, aber nicht geöffnet
    Es wird nichts geschrieben

--- FM3D-Designer/src/WindowLayouts/CreateProject.xaml.cs
+++ FM3D-Designer/src/WindowLayouts/CreateProject.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,6 +16,11 @@ using Microsoft.Win32;
 
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
+using System.Windows.Forms;
+using System.IO;
+
+using System.Collections.ObjectModel;
+using System.Xml;
 
 namespace FM3D_Designer.src.WindowLayouts
 {
@@ -31,14 +35,37 @@ namespace FM3D_Designer.src.WindowLayouts
             InitializeComponent();
             this.Header = "Create Project";
 
+            //var result = await((MetroWindow)Application.Current.MainWindow).ShowMessageAsync("Deleeete", "This will delete the file/directory for ever!", MessageDialogStyle.AffirmativeAndNegative);
+            //if (result == MessageDialogResult.Affirmative)
+            //{ }
+
             this.Initialize(mainWindow, null);
         }
 
         public void bt_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            if (openFileDialog.ShowDialog() == true)
-                tb_path.Text = openFileDialog.FileName;
+            FolderBrowserDialog openbrowserdialog = new FolderBrowserDialog();
+
+            if (openbrowserdialog.ShowDialog() == DialogResult.OK)
+                tb_path.Text = openbrowserdialog.SelectedPath;
         }
+        public void startdev(object sender, RoutedEventArgs e)
+        {
+
+            //XmlWriter writer = new XmlWriter();
+            string path = tb_path.Text + @"\" + tb_name.Text;
+            string projfiles = path + @"\" + @"ProjectFiles";
+            string file = path + @"\" + tb_name.Text + ".fmproj";
+
+            Directory.CreateDirectory(path);//path);
+            Directory.CreateDirectory(projfiles);//projfiles);
+            File.Create(file);
+            mainWindow.ClearAttachedWindows();
+
+            Project.Load(file);
+
+            mainWindow.AttachNewWindowLayout(new MainLayout(this.mainWindow), true);
+        }
+
     }
 }
