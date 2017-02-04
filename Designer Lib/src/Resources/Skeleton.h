6da531e5984fab8d92ab19cfa168d07abe350a6e
Dyrax <dyrax999@gmail.com>
2017-02-04 15:59:19 +0100

    Fixed Skeleton.h and added Allosaurus

---
+++ Designer Lib/src/Resources/Skeleton.h
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace FM3D {
+	class Skeleton;
+}
+
+using namespace System::Collections::ObjectModel;
+
+namespace DesignerLib {
+
+	public ref class Bone {
+	public:
+		property ObservableCollection<Bone^>^ Children;
+		property System::String^ Name;
+	};
+
+	public ref class Skeleton {
+	private:
+		FM3D::Skeleton* m_skeleton;
+	public:
+		property int BoneCount;
+		property System::String^ Name;
+
+		Skeleton(int boneCount, System::String^ name, FM3D::Skeleton* skeleton) {
+			this->m_skeleton = skeleton;
+			this->BoneCount = boneCount;
+			this->Name = name;
+		}
+
+		FM3D::Skeleton* GetIntern() { return m_skeleton; }
+	};
+}
